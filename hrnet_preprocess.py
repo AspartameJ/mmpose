db1876393beff3f7f951838f3d3181416e6e2c4d
AspartameJ <1054136235@qq.com>
2023-04-06 12:38:14 +0800

    预处理嵌套函数

--- hrnet_preprocess.py
+++ hrnet_preprocess.py
@@ -78,67 +78,70 @@ def main():
     # resized_shape_map = {}
     # fliped_shape_map = {}
     # process each image
-    for image_name in mmcv.track_iter_progress(image_list):
-    # for image_name in image_list:
-        cfg = pose_model.cfg
-        device = next(pose_model.parameters()).device
-        if device.type == 'cpu':
-            device = -1
 
-        # build the data pipeline
-        test_pipeline = Compose(cfg.test_pipeline)
+    preprocess(image_list)
 
-        # prepare data
-        data = {
-            'dataset': dataset_name,
-            'ann_info': {
-                'image_size': np.array(cfg.data_cfg['image_size']),
-                'heatmap_size': cfg.data_cfg.get('heatmap_size', None),
-                'num_joints': cfg.data_cfg['num_joints'],
-                'flip_index': flip_index,
-                'skeleton': skeleton,
+    def preprocess(image_list):
+        for image_name in mmcv.track_iter_progress(image_list):
+        # for image_name in image_list:
+            cfg = pose_model.cfg
+            device = next(pose_model.parameters()).device
+            if device.type == 'cpu':
+                device = -1
+
+            # build the data pipeline
+            test_pipeline = Compose(cfg.test_pipeline)
+
+            # prepare data
+            data = {
+                'dataset': dataset_name,
+                'ann_info': {
+                    'image_size': np.array(cfg.data_cfg['image_size']),
+                    'heatmap_size': cfg.data_cfg.get('heatmap_size', None),
+                    'num_joints': cfg.data_cfg['num_joints'],
+                    'flip_index': flip_index,
+                    'skeleton': skeleton,
+                }
             }
-        }
-    
-        if isinstance(image_name, np.ndarray):
-            data['img'] = image_name
-        else:
-            data['image_file'] = image_name
-
-        data = test_pipeline(data)
-        data = collate([data], samples_per_gpu=1)
-        data = scatter(data, [device])[0]        
-
-        ###############################################################
-        img_metas = data["img_metas"] #forward_val(..,img_metas,..)
-        img = data['img'] #forward_val(..,img,..)
-        ###############################################################
-        assert img.size(0) == 1
-        assert len(img_metas) == 1
-
-        img_metas = img_metas[0]
-        aug_data = img_metas['aug_data']
         
-        image_resized = aug_data[0].to(img.device)
-        image_fliped = torch.flip(image_resized, [3]).numpy()
-        image_resized = image_resized.numpy()
+            if isinstance(image_name, np.ndarray):
+                data['img'] = image_name
+            else:
+                data['image_file'] = image_name
 
-        # resized_shape_map[image_resized.shape] = 1
-        # fliped_shape_map[image_fliped.shape] = 1
+            data = test_pipeline(data)
+            data = collate([data], samples_per_gpu=1)
+            data = scatter(data, [device])[0]        
 
-    # for key, value in resized_shape_map.items():
-    #     print(key)
-    # print('=====================================================')
-    # for key, value in fliped_shape_map.items():
-    #     print(key)
+            ###############################################################
+            img_metas = data["img_metas"] #forward_val(..,img_metas,..)
+            img = data['img'] #forward_val(..,img,..)
+            ###############################################################
+            assert img.size(0) == 1
+            assert len(img_metas) == 1
 
+            img_metas = img_metas[0]
+            aug_data = img_metas['aug_data']
+            
+            image_resized = aug_data[0].to(img.device)
+            image_fliped = torch.flip(image_resized, [3]).numpy()
+            image_resized = image_resized.numpy()
 
-        npy_filename = '{}.npy'.format(image_name.split('/')[-1].split('.')[0])
-        output_path_resized = os.path.join(args.resized_img_root, npy_filename)
-        output_path_fliped = os.path.join(args.fliped_img_root, npy_filename)
-        
-        np.save(output_path_resized, image_resized)
-        np.save(output_path_fliped, image_fliped)
+            # resized_shape_map[image_resized.shape] = 1
+            # fliped_shape_map[image_fliped.shape] = 1
+
+        # for key, value in resized_shape_map.items():
+        #     print(key)
+        # print('=====================================================')
+        # for key, value in fliped_shape_map.items():
+        #     print(key)
+
+            npy_filename = '{}.npy'.format(image_name.split('/')[-1].split('.')[0])
+            output_path_resized = os.path.join(args.resized_img_root, npy_filename)
+            output_path_fliped = os.path.join(args.fliped_img_root, npy_filename)
+            
+            np.save(output_path_resized, image_resized)
+            np.save(output_path_fliped, image_fliped)
 
 
 
