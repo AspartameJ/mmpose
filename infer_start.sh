315d1382e2de832413d981a3cf3394196bdf937a
AspartameJ <1054136235@qq.com>
2023-04-07 10:08:37 +0800

    new log dir

--- infer_start.sh
+++ infer_start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+CURRENT_DIR=`pwd`
+
+start_time=$(date +%s)
+
+if [ -d ${CURRENT_DIR}/log ]; then
+    cp -rf ${CURRENT_DIR}/log ${CURRENT_DIR}/log_${start_time}
+    rm -rf ${CURRENT_DIR}/log
+    mkdir -p ${CURRENT_DIR}/log
+else
+    mkdir -p ${CURRENT_DIR}/log
+fi
 
 python3 hrnet_om_infer_end.py &
 
--- monitor.sh
+++ monitor.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 test_path_dir=`pwd`
 RANK_ID=$1
-
-if [ ! -d ${test_path_dir}/output/${RANK_ID} ];then
-  mkdir -p ${test_path_dir}/output/${RANK_ID}
-fi
+CODE_NAME="hrnet_om_infer_end.py"
 
 for i in $(seq 1 500000)
 do
-  #job_num=`ps -efww|grep benchmark |grep -v grep|wc -l`
-  job_num=`ps -efww|grep -v grep|grep -c "hrnet_om_infer_end.py"`
+  job_num=`ps -efww|grep -v grep|grep -c "$CODE_NAME"`
   if [ ${job_num} -eq 0 ]
   then
-    echo "infer done-------------------"
     break
   fi
-  #echo "Record the status of  CPU NPU MEM"
   time=`date |awk '{print $4}'`
-  mpstat >>${test_path_dir}/output/${RANK_ID}/cpu_monitor.log &
-  echo "$time " `npu-smi info | grep -E "/" | awk -v line=$((2*RANK_ID+1)) 'NR==line{printf "NPU=%s,   Power(W)=%s,\tTemp(C):%s   HBM-Usage(MB):%s/%s,  ",$3,$7,$8,$9,$11};NR==line+1{printf "AICore(%%):%s,   Memory-Usage(MB):%s/%s  ",$7,$8,$10}'` >>${test_path_dir}/output/${RANK_ID}/ascend_monitor.log &
-  echo  "$time "`free |grep Mem` >> ${test_path_dir}/output/${RANK_ID}/mem_monitor.log &
+  mpstat 1 1 >> ${CURRENT_DIR}/log/cpu_monitor.log &
+  echo "$time " `npu-smi info | grep -E "/" | awk -v line=$((2*RANK_ID+1)) 'NR==line{printf "NPU=%s,   Power(W)=%s,\tTemp(C):%s   HBM-Usage(MB):%s/%s,  ",$3,$7,$8,$9,$11};NR==line+1{printf "AICore(%%):%s,   Memory-Usage(MB):%s/%s  ",$7,$8,$10}'` >>${test_path_dir}/log/ascend_monitor.log &
+  # echo "$time " `npu-smi info | grep -E "/" | awk -v line=$((2*RANK_ID+1)) 'NR==line{printf "NPU=%s,   Power(W)=%s,\tTemp(C):%s,  ",$3,$7,$8};NR==line+1{printf "AICore(%%):%s,   Memory-Usage(MB):%s/%s, HBM-Usage(MB):%s/%s",$6,$7,$9,$10,$12}'` >>${test_path_dir}/log/ascend_monitor.log &
+  echo  "$time "`free |grep Mem` >> ${test_path_dir}/log/mem_monitor.log &
   sleep 1
 done
