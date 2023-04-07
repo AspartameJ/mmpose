20d70fd73b5264751f6c2f507741cb8f2cf97657
AspartameJ <1054136235@qq.com>
2023-04-07 10:17:07 +0800

    debug

--- monitor.sh
+++ monitor.sh
@@ -11,7 +11,7 @@ do
     break
   fi
   time=`date |awk '{print $4}'`
-  mpstat 1 1 >> ${CURRENT_DIR}/log/cpu_monitor.log &
+  mpstat 1 1 >> ${test_path_dir}/log/cpu_monitor.log &
   echo "$time " `npu-smi info | grep -E "/" | awk -v line=$((2*RANK_ID+1)) 'NR==line{printf "NPU=%s,   Power(W)=%s,\tTemp(C):%s   HBM-Usage(MB):%s/%s,  ",$3,$7,$8,$9,$11};NR==line+1{printf "AICore(%%):%s,   Memory-Usage(MB):%s/%s  ",$7,$8,$10}'` >>${test_path_dir}/log/ascend_monitor.log &
   # echo "$time " `npu-smi info | grep -E "/" | awk -v line=$((2*RANK_ID+1)) 'NR==line{printf "NPU=%s,   Power(W)=%s,\tTemp(C):%s,  ",$3,$7,$8};NR==line+1{printf "AICore(%%):%s,   Memory-Usage(MB):%s/%s, HBM-Usage(MB):%s/%s",$6,$7,$9,$10,$12}'` >>${test_path_dir}/log/ascend_monitor.log &
   echo  "$time "`free |grep Mem` >> ${test_path_dir}/log/mem_monitor.log &
